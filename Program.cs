aa013143c64b3fab38fd89fa2211f0907a8e35b4
Joby John <joby432john@gmail.com>
2022-08-12 10:46:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("And this is a simple calculator\n");
+Console.Write("Enter first number ");
+double firstnum = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the operator ");
+string op = Console.ReadLine();
+Console.Write("Enter second number");
+double secondnum = Convert.ToDouble(Console.ReadLine());
+switch(op)
+{
+    case "+":
+        Console.WriteLine("Result : " + (firstnum + secondnum));
+        break;
+    case "-":
+        Console.WriteLine("Result : " + (firstnum - secondnum));
+        break;
+    case "/":
+        Console.WriteLine("Result : " + (firstnum / secondnum));
+        break;
+    case "*":
+        Console.WriteLine("Result : " + (firstnum * secondnum));
+        break;
+    case "%":
+        Console.WriteLine("Result : " + (firstnum % secondnum));
+        break;
+        default:
+        Console.WriteLine("Enter a valid operator");
+        break;
+}
